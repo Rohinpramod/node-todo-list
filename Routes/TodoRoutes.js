fe63a81402e9b88087959f069659e66d4e23f8f3
ROHIN PRAMOD <rohinpramod320@gmail.com>
2024-09-09 11:36:26 +0530

    added todo

--- Routes/TodoRoutes.js
+++ Routes/TodoRoutes.js
@@ -8,8 +8,8 @@ const validateTodoList = require('../middlewares/validateTodoList');
 router.post('/',validateTodoList,todoController.addNewTodo);
 router.get('/todo',todoController.getAllTodo);
 router.get('/',(req,res)=>{res.send("welcome to ToDo App")})
-router.get('/:id',todoController.getTodoById);
-router.put('/:id',validateTodoList,todoController.UpdateTodo);
-router.delete('/:id',todoController.deleteTodo);
+router.get('/todo/:id',todoController.getTodoById);
+router.put('/todo/:id',validateTodoList,todoController.UpdateTodo);
+router.delete('/todo/:id',todoController.deleteTodo);
 
 module.exports = router;
